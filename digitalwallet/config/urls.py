436664a5a427cb738596a4fd4c15d254102f5453
Mansi-Chauhan27 <mansic@mindfiresolutions.com>
2021-07-27 12:37:04 +0530

    Static Files

--- digitalwallet/config/urls.py
+++ digitalwallet/config/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
+from django.conf.urls.static import static
+
+from django.conf import settings
+
+
+
 
 
 urlpatterns = [
@@ -26,3 +32,5 @@ urlpatterns = [
     path('devices/', include('apps.devices.urls')),
     
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
